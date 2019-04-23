ebc527bb1fa09caad2beea71c431b271f0653d02
MapleCCC <littlelittlemaple@gmail.com>
2019-04-24 03:58:44 +0800

    Slight change to model schema

--- transliterator/models.py
+++ transliterator/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 
 class UserDict(models.Model):
-    chinese_words = models.CharField(
+    chinese_word = models.CharField(
         max_length=25, primary_key=True, unique=True)
     pinyin = models.CharField(max_length=50)
     count = models.IntegerField(default=1)
 
     def __str__(self):
-        return self.chinese_words
+        return self.chinese_word
 
 
 # class BaseDict(models.Model):
