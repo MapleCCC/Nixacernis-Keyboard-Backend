5ea6b2792cc2e66683d47559aa6b0df826fd050d
MapleCCC <littlelittlemaple@gmail.com>
2019-04-24 02:18:49 +0800

    Create and Activate DB model for transliterator

--- transliterator/models.py
+++ transliterator/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class UserDict(models.Model):
+    chinese_words = models.CharField(
+        max_length=25, primary_key=True, unique=True)
+    pinyin = models.CharField(max_length=50)
+    count = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.chinese_words
+
+
+# class BaseDict(models.Model):
+#     pinyin = models.CharField(max_length=50)
+#     chinese_words = models.CharField(max_length=25)
+#     count = models.IntegerField(default=1)
